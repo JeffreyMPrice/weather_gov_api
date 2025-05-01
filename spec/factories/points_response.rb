f63581aa4d3873a523a1cd893acc25d80b512c9e
Jeffrey Price <jeff.price@gmail.com>
2025-04-30 20:45:43 -0400

    [15] Fix end of line, add .gitattributes, rubocop fixes

--- spec/factories/points_response.rb
+++ spec/factories/points_response.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :points_response, class: Hash do
     transient do
--- spec/factories/stations_response.rb
+++ spec/factories/stations_response.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :stations_response, class: Hash do
     transient do
